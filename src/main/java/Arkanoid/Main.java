0f4cd39d30219cca4f591d10ba331af6e5a35963
otdauto <24022823@vnu.edu.vn>
2025-09-25 15:36:22 +0700

    add javafx

---
+++ src/main/java/Arkanoid/Main.java
@@ -0,0 +1,23 @@
+package Arkanoid;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage stage) {
+        Label label = new Label("Hello Arkanoid!");
+        StackPane root = new StackPane(label);
+        Scene scene = new Scene(root, 800, 600);
+        stage.setTitle("Arkanoid Test");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
